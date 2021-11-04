5346b9e5c06a74df97134e37862316f9616de154
BEU <evbolohov@yandex.ru>
2021-11-04 19:48:33 +0300

    1

--- Занятие2/Домашнее задание/task3/main.py
+++ Занятие2/Домашнее задание/task3/main.py
@@ -1,1 +1,10 @@
+if __name__ == "__main__":
+    A = int(input())
+    B = int(input())
+    C = int(input())
+
+    if ((A < 45) and (B >= 45) and (C >= 45)) or ((B < 45) and (C >= 45) and (A >= 45)) or ((C < 45) and (B >= 45) and (A >= 45)):
+        print(True)
+    else:
+        print(False)
 # TODO
