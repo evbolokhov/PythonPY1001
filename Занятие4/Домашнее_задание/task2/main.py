ec8d5ee53b2d4344e6fbf55bcb2e5c111d139a9b
BEU <evbolohov@yandex.ru>
2021-11-05 01:53:08 +0300

    end

--- Занятие4/Домашнее_задание/task2/main.py
+++ Занятие4/Домашнее_задание/task2/main.py
@@ -1,3 +1,9 @@
+def equal_number(n):
+    len__ = len([int(_) for _ in str(n)])
+    len_ = len(set([int(_) for _ in str(n)]))
+    return print('Есть одинаковые') if len_ != len__ else print('Нет одинаковых')
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    N = input('Введите число: ')
+    equal_number(N)
--- Занятие4/Домашнее_задание/task4/main.py
+++ Занятие4/Домашнее_задание/task4/main.py
@@ -1,3 +1,7 @@
+def happiness(n):
+    return print('Счастливое') if (sum([int(_) for _ in str(n)][:3])) == (sum([int(_) for _ in str(n)][3:])) else print('Не счастливое')
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    N = 123321
+    happiness(N)
