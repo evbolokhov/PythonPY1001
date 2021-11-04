c372afad8d835b9c340511e39704e0b0b545cab5
BEU <evbolohov@yandex.ru>
2021-11-05 02:24:25 +0300

    end

--- Занятие5/Домашнее_задание/task1/main.py
+++ Занятие5/Домашнее_задание/task1/main.py
@@ -1,3 +1,6 @@
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    for i in range(1, 10):
+        for j in range(1, i + 1):
+            k = i * j
+            print("{0}*{1}={2}".format(i, j, k), end=" ")
+        print("")
