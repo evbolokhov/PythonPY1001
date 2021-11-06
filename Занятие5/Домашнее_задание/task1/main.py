92674f99ca1495c39b5973f62cad379b79184efa
BEU <evbolohov@yandex.ru>
2021-11-06 13:25:42 +0300

    end

--- Занятие5/Домашнее_задание/task1/main.py
+++ Занятие5/Домашнее_задание/task1/main.py
@@ -1,6 +1,16 @@
 if __name__ == "__main__":
-    for i in range(1, 10):
-        for j in range(1, i + 1):
-            k = i * j
-            print("{0}*{1}={2}".format(i, j, k), end=" ")
-        print("")
+    i = 15
+
+    length = len(str(i**2)) + 3
+    width = len(str(i)) + 3
+
+    headers = " " * width + " " + "".join([str(j).rjust(length) for j in range(1, i + 1)])
+    dashes = " " * width + ":" + "-" * length * i
+    body = ""
+
+    for row in range(1, i+1):
+        body += str(row).rjust(width) + ":"
+        for column in range(1, i+1):
+            body += str(row * column).rjust(length)
+        body += "\n"
+    print("\n".join([headers, dashes, body]))
